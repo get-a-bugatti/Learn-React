d9980a194a9bcf95bf1a6c54ce6ecd6aa1384cf3
get-a-bugatti <37701415+get-a-bugatti@users.noreply.github.com>
2026-02-16 10:20:07 +0545

    finished project 6 - currency convereter

---
+++ 06-currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((out) => {
+        setData(out[currency]);
+      })
+      .catch((err) => console.log(err));
+  }, [currency]);
+
+  return data;
+}
+
+export default useCurrencyInfo;
